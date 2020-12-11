101e9067e11d87acd7796fa4bf3362c52e3131bc
Ola <olawunmi263@yahoo.com>
2020-12-11 03:36:22 +0000

    Basepage added

--- src/test/java/com/Moonpig/pages/BasePage.java
+++ src/test/java/com/Moonpig/pages/BasePage.java
@@ -2,7 +2,10 @@ package com.Moonpig.pages;
 
 import com.Moonpig.commons.DriverLibrary;
 import io.cucumber.java.eo.Se;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage extends DriverLibrary {
     String BASE_URL = "https://www.moonpig.com/uk/";
@@ -10,5 +13,21 @@ public class BasePage extends DriverLibrary {
 
     public void launchURL(){
         driver.navigate().to(BASE_URL);
+    }
+    public void selectByValue(WebElement element, String value){
+        select = new Select(element);
+        select.selectByValue(value);
+    }
+    public void selectByText(WebElement element, String text){
+        select = new Select(element);
+        select.selectByVisibleText(text);
+    }
+    public void selectByIndex(WebElement element, int index){
+        select  = new Select(element);
+        select.selectByIndex(index);
+    }
+    public void waitForElementToBeDisplayed(WebElement element){
+        wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 }
